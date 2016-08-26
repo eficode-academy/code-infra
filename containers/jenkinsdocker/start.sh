ae1e835cf51cc55ae8b5afb8387de00d62bcadb5
hoeghh <heh@praqma.net>
2016-08-26 12:03:37 +0200

    Upgraded docker to latest version

--- containers/jenkinsdocker/start.sh
+++ containers/jenkinsdocker/start.sh
@@ -6,8 +6,14 @@ function generateDockerfile(){
 cat <<EOF
 from jenkins:2.7.2
 USER root
+RUN apt-get update
+RUN apt-get install apt-transport-https ca-certificates software-properties-common -y
+RUN apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+RUN add-apt-repository 'deb https://apt.dockerproject.org/repo debian-jessie main'
 RUN apt-get update -y
-RUN apt-get install docker.io -y
+RUN apt-cache policy docker-engine
+RUN apt-get update -y
+RUN apt-get install docker-engine -y
 RUN groupadd -g $dockerid dockerg
 RUN usermod -a -G dockerg jenkins
 USER jenkins
@@ -23,4 +29,4 @@ docker build -t dockerjenkins:1 .
 
 echo "Starting Jenkins with Docker"
 
-docker run -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /data/jenkins:/var/jenkins_home dockerjenkins:1
+docker run -d -p 8085:8080 -p 50005:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /data/jenkins:/var/jenkins_home dockerjenkins:1
