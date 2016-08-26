ec972d4350541103d0773bf1198acdbc481bdce4
hoeghh <heh@praqma.net>
2016-08-26 10:55:40 +0200

    Simple jenkins with local docker for jenkinsuser

---
+++ containers/jenkinsdocker/start.sh
@@ -0,0 +1,26 @@
+dockerid=$(getent group docker|cut -d":" -f3)
+echo "Docker group id on host : $dockerid"
+
+function generateDockerfile(){
+
+cat <<EOF
+from jenkins:2.7.2
+USER root
+RUN apt-get update -y
+RUN apt-get install docker.io -y
+RUN groupadd -g $dockerid dockerg
+RUN usermod -a -G dockerg jenkins
+USER jenkins
+EOF
+
+}
+
+echo "Createing Dockerfile"
+generateDockerfile > Dockerfile
+
+echo "Building Jenkins Docker image"
+docker build -t dockerjenkins:1 .
+
+echo "Starting Jenkins with Docker"
+
+docker run -d -p 85:8080 -p 50001:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /data/jenkins:/var/jenkins_home dockerjenkins:1
