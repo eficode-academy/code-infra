c4d4c7e72b4ef16f2f3f253ee745b60772fe6f50
Mike Long <mike@praqma.com>
2016-09-07 13:01:48 +0000

    Move jenkins home directory to be same path on host and target

--- containers/create-homes.sh
+++ containers/create-homes.sh
@@ -4,11 +4,12 @@ echo "You should not be running any containers in /opt/containers when you run t
 echo "Removing old directory structure from /opt/containers"
 rm /opt/containers/* -fr
 echo "Re-creating directory structure inside /opt/containers"
-mkdir -p /opt/containers/jenkins_home 
+mkdir -p /var/jenkins_home 
 mkdir -p /opt/containers/artifactory/data
 mkdir -p /opt/containers/artifactory/logs
 mkdir -p /opt/containers/artifactory/backup
 mkdir -p /opt/containers/registry
 echo "Changing ownership of /opt/containers to 1000:50 recursively"
+chown 1000:50 /var/jenkins_home -R
 chown 1000:50 /opt/containers -R
 echo "Done."
